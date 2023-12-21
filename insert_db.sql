c8a1b7a5bb2ea022f0571e1c34ee086ae7fe6460
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-12-21 11:55:01 +0100

    debugged and created DB

--- insert_db.sql
+++ insert_db.sql
@@ -1,24 +1,27 @@
-CREATE TABLE departments(
+CREATE TABLE departments (
     id INTEGER PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE employees(
+CREATE TABLE employees (
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL, 
     last_name VARCHAR(255) NOT NULL,
-    department_id INTEGER NOT NULL
+    department_id INTEGER NOT NULL,
 
     FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 INSERT INTO 
-    departments(name)
+    departments (name)
 VALUES
-    ("Managers"), ("Bartenders"), ("Waiters"), ("Kitchen");
+    ("Managers"),
+    ("Bartenders"), 
+    ("Waiters"),
+    ("Kitchen");
 
 INSERT INTO 
-    employees(first_name, last_name, department_id)
+    employees (first_name, last_name, department_id)
 VALUES
     ("Adam", "Smith", (SELECT id FROM departments WHERE name = "Managers") ),
     ("Gloria", "Adams", (SELECT id FROM departments WHERE name = "Managers")),
@@ -29,4 +32,4 @@ VALUES
     ("David", "Khaz", (SELECT id FROM departments WHERE name = "Waiters")),
     ("Anna", "Kowalski", (SELECT id FROM departments WHERE name = "Waiters")),
     ("Brian", "Ramirez", (SELECT id FROM departments WHERE name = "Kitchen")),
-    ("Angela", "Sailor", (SELECT id FROM departments WHERE name = "Kitchem"));
+    ("Angela", "Sailor", (SELECT id FROM departments WHERE name = "Kitchen"));
