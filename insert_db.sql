78d5011369d8d53dd95248b029311084c2b2ca34
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-12-21 11:46:42 +0100

    inserting data

--- insert_db.sql
+++ insert_db.sql
@@ -11,4 +11,22 @@ CREATE TABLE employees(
 
     FOREIGN KEY (department_id) REFERENCES departments(id)
 );
-    
+
+INSERT INTO 
+    departments(name)
+VALUES
+    ("Managers"), ("Bartenders"), ("Waiters"), ("Kitchen");
+
+INSERT INTO 
+    employees(first_name, last_name, department_id)
+VALUES
+    ("Adam", "Smith", (SELECT id FROM departments WHERE name = "Managers") ),
+    ("Gloria", "Adams", (SELECT id FROM departments WHERE name = "Managers")),
+    ("Philip", "Rezno", (SELECT id FROM departments WHERE name = "Bartenders")),
+    ("Natalie", "de La Cruz", (SELECT id FROM departments WHERE name = "Bartenders")),
+    ("Victoria", "Miles", (SELECT id FROM departments WHERE name = "Bartenders")), 
+    ("Melanie", "Prost", (SELECT id FROM departments WHERE name = "Waiters")),
+    ("David", "Khaz", (SELECT id FROM departments WHERE name = "Waiters")),
+    ("Anna", "Kowalski", (SELECT id FROM departments WHERE name = "Waiters")),
+    ("Brian", "Ramirez", (SELECT id FROM departments WHERE name = "Kitchen")),
+    ("Angela", "Sailor", (SELECT id FROM departments WHERE name = "Kitchem"));
